37513699778f4e167fb9bf63d4d329a7d4c7e5a8
blnkingso532 <714232542@qq.com>
2025-08-25 16:19:17 +0800

    fix warning

--- src/constant.rs
+++ src/constant.rs
@@ -1,5 +1,6 @@
 /// Default multipart size (4MB)
 pub(crate) const MULTIPART_SIZE: u32 = 4 << 20;
 
+#[allow(dead_code)]
 /// 默认buffer大小（512KB）
 pub(crate) const DEFAULT_BUFFER_SIZE: usize = 512 << 10;
